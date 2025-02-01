109b99c0f0f2daf9c8d2830eccc7c8a3bf7179d9
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2025-02-01 18:06:30 +0100

    Use new apis

--- test/functional/shada/shada_spec.lua
+++ test/functional/shada/shada_spec.lua
@@ -4,9 +4,6 @@ local n = require('test.functional.testnvim')()
 local t_shada = require('test.functional.shada.testutil')
 local uv = vim.uv
 local paths = t.paths
-local helpers = require('test.functional.helpers')(after_each)
-local meths, curbufmeths, funcs, retry =
-    helpers.meths, helpers.curbufmeths, helpers.command, helpers.funcs, helpers.eq, helpers.retry
 
 local api, nvim_command, fn, eq = n.api, n.command, n.fn, t.eq
 local write_file, set_session, exc_exec = t.write_file, n.set_session, n.exc_exec
@@ -217,20 +214,18 @@ describe('ShaDa support code', function()
   end)
 
   it('does not store unlisted buffer', function()
-    local fname = funcs.getcwd() .. '/file'
-    meths.set_var('__fname', fname)
+    local fname = fn.getcwd() .. '/file'
+    api.nvim_set_var('__fname', fname)
     nvim_command('edit `=__fname`')
-    curbufmeths.set_option('buflisted', false)
+    api.nvim_set_option_value('buflisted', false, {})
     nvim_command('wshada! ' .. shada_fname)
 
     eq({}, find_file(fname))
 
     -- Set 'buflisted', then check again.
-    curbufmeths.set_option('buflisted', true)
+    api.nvim_set_option_value('buflisted', true, {})
     nvim_command('wshada! ' .. shada_fname)
-    retry(nil, 4000, function()
-      eq({ [7] = 1, [8] = 1, [10] = 1 }, find_file(fname))
-    end)
+    eq({ [7] = 1, [8] = 1, [10] = 1 }, find_file(fname))
   end)
 
   it('is able to set &shada after &viminfo', function()
